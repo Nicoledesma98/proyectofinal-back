2b97fdd44feb3f5f2214b01b16fb8ab5f91382c9
nicolas ledesma <nico-ledesma98@hotmail.es>
2023-02-13 16:59:34 -0300

    arreglando erroress

--- src/index.js
+++ src/index.js
@@ -1,39 +1,42 @@
-import express  from "express";
-import {ProductManager} from "./controllers/ProductManager.js"
+const express =require("express") ;
+const ProductManager = require("./models/ProductManager.js")
+
+const productManager = new ProductManager()
+productManager.resetTxt()
 
 const app = express()
 const PORT = 4000
 
 
-const productManager = new ProductManager('src/models/productos.txt')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-//routes
-// app.use('/api/product', prodRouter)
+
 app.get('/product/:id',async (req,res) =>{
     const productos = await productManager.getProducts()
-    console.log(await productManager.getProducts()) ///llegan todos los productos
     const prueba  = productos.find( prod => prod.id === parseInt(req.params.id))
     console.log("esto es prueba",prueba)
     console.log(req.params.id)
-    return res.status(200).json(prueba)
+    return res.send(prueba)
 })
 app.get('/product', async (req, res) => {
     const products = await productManager.getProducts();
+    console.log("esto es products en appget", products)
     const limit = req.query.limit ? parseInt(req.query.limit) : products.length;
     const limitedProducts = products.slice(0, limit);
-    console.log("esto es limitedproducts",limitedProducts)
-    return res.status(200).json(limitedProducts);
+    console.log("esti es limitedproducts", limitedProducts)
+    return res.send(limitedProducts);
   });
+
   app.post('/product', async (req,res) =>{
-    let producto = await productManager.addProduct(req.body)
+    const producto = await productManager.addProduct(req.body)
+    console.log("esto es products en apppost", producto)
     res.send(producto)
 })
 app.delete('/product/:id', async (req, res) => {
-    let producto = await productManager.deleteProduct(req.params.id) 
-    res.send(producto)
+    const productos = await productManager.deleteProduct(req.params.id) 
+    res.send(productos)
 })
 app.put('/product/:id', async (req,res)=>{
     let producto = await productManager.updateProduct(req.params.id, req.body)
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,23 +1,20 @@
 // import { Router } from "express";
-// import { ProductManager } from "../controllers/productManager";
+// import  ProductManager  from "../controllers/ProductManager.js";
 
-// const productManager = new ProductManager('src/models/productos.txt')
+// const productManager = new ProductManager()
 // const prodRouter = Router()
 
 // prodRouter.get('/:id',async (req,res) =>{
 //     const productos = await productManager.getProducts()
-//     console.log(await productManager.getProducts()) ///llegan todos los productos
 //     const prueba  = productos.find( prod => prod.id === parseInt(req.params.id))
-//     console.log("esto es prueba",prueba)
 //     console.log(req.params.id)
-//     return res.status(200).json(prueba)
+//     return res.send(JSON.stringify(prueba))
 // })
 // prodRouter.get('/', async (req, res) => {
 //     const products = await productManager.getProducts();
 //     const limit = req.query.limit ? parseInt(req.query.limit) : products.length;
 //     const limitedProducts = products.slice(0, limit);
-//     console.log("esto es limitedproducts",limitedProducts)
-//     return res.status(200).json(limitedProducts);
+//     return res.send(JSON.stringify(limitedProducts));
 //   });
 // prodRouter.post('/', async (req,res) =>{
 //     let producto = await productManager.addProduct(req.body)
